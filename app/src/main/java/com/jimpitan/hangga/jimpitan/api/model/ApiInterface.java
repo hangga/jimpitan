6c6ae02a7f9b90ae774fc36cbdcbe690a5e4d5e0
sayekti <sayekti@sebangsa.com>
2018-08-09 16:14:46 +0700

    sip

--- app/src/main/java/com/jimpitan/hangga/jimpitan/api/model/ApiInterface.java
+++ app/src/main/java/com/jimpitan/hangga/jimpitan/api/model/ApiInterface.java
@@ -15,22 +15,6 @@ public interface ApiInterface {
     Call<Getwarga> getWarga();
 
 
-    /*@GET("AKfycbzrUOP5IYWEsNfMP6YkkdowGGetZatt36--txlsHtDnbtudnXB1/exec?sheet=config")
-    Call<Getconfig> getConfig();
-
-    @FormUrlEncoded
-    @POST("AKfycbwZfFSifiACnWfWO2g60I9Y5ItmQD2I-zzBoprs64R8Xa8eCto/exec")
-    Call<PostJimpitan> postJimpitan(@Field("idjimp") String idjimp,
-                                  @Field("hari") String hari,
-                                  @Field("tanggal") String tanggal,
-                                  @Field("bulan") String bulan,
-                                  @Field("tahun") String tahun,
-                                  @Field("jam") String jam,
-                                  @Field("nama") String nama,
-                                  @Field("nominal") int nominal,
-                                  @Field("submitby") String submitby
-    );*/
-
     @FormUrlEncoded
     @POST("AKfycbyPZ7AneiiqCLAo0wxxY5u8zj_FYxJOkkqX5gqiJ5gNXXoanuw/exec")
     Call<PostJimpitan> postJimpitanNew(@Field("idjimp") String idjimp,
